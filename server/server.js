bc60a0ea2faa10d14af2eb5e1ff39207686d4d6a
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-27 13:24:19 -0500

    server comments

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
-// requires
+// require express - gives me a function
 var express = require('express');
-var bodyParser = require('body-parser');
-var aardvarkRouter = require('./routes/aardvarkRouter.js');
-// global
+// create an instance of express by calling the function above - gives me an object
 var app = express();
-var port = process.env.PORT || 5002;
-// uses
-app.use(express.static('server/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+// a way of redirecting internet traffic
+var port = process.env.PORT || 5000;
+// body parser is a module that helps us by turning our response body into an object
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));// <- req.body won't work without this
+// require the router 
+var aardvarkRouter = require('./routes/aardvarkRouter.js');
 app.use('/aardvark', aardvarkRouter);
+// express static file serving - public is the folder name
+app.use(express.static('server/public'));
 // start up server
 app.listen(port, function(){
     console.log('Server running on port:', port);  
